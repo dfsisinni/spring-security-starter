37ed87ea62f1b0453f6e8d02295efbf6027ac770
Dane Sisinni <dfsisinni@gmail.com>
2018-03-05 22:07:41 -0500

    revised App.java for AWS deployment

--- src/main/java/com/starter/App.java
+++ src/main/java/com/starter/App.java
@@ -1,12 +1,23 @@
 package com.starter;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(App.class).bannerMode(Banner.Mode.OFF);
 	}
 }
